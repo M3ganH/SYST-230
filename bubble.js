815cc2f72c1cc847cad3e9066af0750ed6c87ce5
Eric <39538689+EricZoop@users.noreply.github.com>
2024-02-23 23:24:17 -0500

    Create bubble.js

---
+++ bubble.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.bubble-button').forEach(button => {
+    button.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        e.target.classList.toggle("animate");
+        setTimeout(function() {
+            e.target.classList.toggle("animate");
+        }, 750);
+    }, false);
+});
